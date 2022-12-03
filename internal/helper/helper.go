93d416485905ddac79c94eb764c98068d796fed0
luckyAkbar <lucky.akbar105619@students.unila.ac.id>
2022-12-03 16:47:09 +0700

    request id on context

--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -2,9 +2,12 @@ package helper
 
 import (
 	"context"
+	"encoding/json"
 	"strings"
 
 	"github.com/google/uuid"
+	"github.com/kumparan/go-utils"
+	"github.com/luckyAkbar/central-worker-service/internal/model"
 	"github.com/sirupsen/logrus"
 )
 
@@ -15,10 +18,33 @@ func WrapCloser(closeFn func() error) {
 	}
 }
 
+func GetRequestIDFromCtx(ctx context.Context) string {
+	id, ok := ctx.Value(model.ReqIDCtxKey).(string)
+	if !ok {
+		logrus.WithField("ctx", utils.DumpIncomingContext).Warn("error getting request id")
+		return ""
+	}
+
+	return id
+}
+
 // DumpContext dump all necessary value in context
 // also dump the custom value written to context, such as user, and request ID
 func DumpContext(ctx context.Context) string {
-	return ""
+	reqID := GetRequestIDFromCtx(ctx)
+
+	data := map[string]any{
+		"request_id": reqID,
+		"any":        utils.DumpIncomingContext(ctx),
+	}
+
+	v, err := json.Marshal(data)
+	if err != nil {
+		logrus.Error(err)
+		return ""
+	}
+
+	return string(v)
 }
 
 // GenerateRequestID generate request ID using UUID v4 and stripping the '-'
