e75ab5e713c8ed2bccc823e5b7ccd56cea6bbc43
luckyAkbar <lucky.akbar105619@students.unila.ac.id>
2022-12-03 16:47:28 +0700

    add new model

--- internal/model/common.go
+++ internal/model/common.go
@@ -1,5 +1,23 @@
 package model
 
+import (
+	validate "github.com/go-playground/validator/v10"
+)
+
 type contextKey string
 
-const ReqIDCtxKey contextKey = "request_id_context_key"
+// context key for request id
+var ReqIDCtxKey contextKey = "request_id_context_key"
+
+var validator = validate.New()
+
+// UsecaseError error returned from usecase
+type UsecaseError struct {
+	UnderlyingError error  `json:"error"`
+	Message         string `json:"message"`
+}
+
+var (
+	// NilUsecaseError returned when no error is returned
+	NilUsecaseError UsecaseError = UsecaseError{}
+)
