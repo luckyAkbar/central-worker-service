9f8311445ada0746a2246987a797fa20f049b211
luckyAkbar <luckyakbar1509@gmail.com>
2022-12-24 11:16:13 +0700

    feat: add new error type

--- internal/usecase/errors.go
+++ internal/usecase/errors.go
@@ -4,12 +4,14 @@ import "errors"
 
 // list of usecase underlying error
 var (
-	ErrValidations = errors.New("validation error")
-	ErrInternal    = errors.New("internal error")
+	ErrValidations   = errors.New("validation error")
+	ErrInternal      = errors.New("internal error")
+	ErrAlreadyExists = errors.New("already exists")
 )
 
 // list of standard error message
 var (
 	MsgDatabaseError      = "operation failed, database error"
 	MsgFailedRegisterTask = "failed to register task"
+	MsgInternalError      = "internal error"
 )
