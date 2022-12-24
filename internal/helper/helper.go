6e075d3f24424f38e994f32ea07834852930f460
luckyAkbar <luckyakbar1509@gmail.com>
2022-12-24 11:15:42 +0700

    feat: add new helper func

--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -2,7 +2,10 @@ package helper
 
 import (
 	"context"
+	"crypto/sha512"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"strings"
 
 	"github.com/google/uuid"
@@ -52,4 +55,38 @@ func DumpContext(ctx context.Context) string {
 func GenerateID() string {
 	id := uuid.New()
 	return strings.ReplaceAll(id.String(), "-", "")
+}
+
+// CreateHashSHA512 generate sha512 hash and return result in base64 encoded string
+func CreateHashSHA512(data []byte) string {
+	sha := sha512.New()
+	sha.Write(data)
+
+	return EncodeBase64(sha.Sum(nil))
+}
+
+// EncodeBase64 encode data to base64 string
+func EncodeBase64(data []byte) string {
+	return base64.StdEncoding.EncodeToString(data)
+}
+
+// DecodeBase64 decode data to []byte and return err if any
+func DecodeBase64(data string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(data)
+}
+
+// HTMLContentForUserRegistrationEmail create html content for user registration email
+func HTMLContentForUserRegistrationEmail(username, url string) string {
+	return fmt.Sprintf(`
+	<html>
+		<h1>Thanks for registering on our platform, Central Service!</h1>
+		<p>Hello, %s!, recently you register your account on our platform. To activate the account, please click link below.</p>
+		<p><a href="%s">activate</a></p>
+		<br><br>
+		<p>But, if you never register on our platform, just ignore this message.</p>
+		<p>Thanks in advance!</p>
+	</html>
+	
+	
+	`, username, url)
 }
